435620ca0c77ed0e55e82d6c1ce21b80520e63b4
Grzesiek <dadmor@gmail.com>
2025-10-15 20:14:41 +0200

    chore: release v1.0.28

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// Main entry point for refelt-design
+// Re-export all uikit components
+export * from './lib/uikit/index.js';
+
+// Re-export all rich components
+export * from './lib/rich/index.js';
